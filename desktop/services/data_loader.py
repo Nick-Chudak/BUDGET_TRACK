65f53cdf7a145560e7e65867b9070b922a878115
Nick-Chudak <nchudak2004@gmail.com>
2025-07-06 12:52:08 -0500

    7/6/2025 work

--- desktop/services/data_loader.py
+++ desktop/services/data_loader.py
@@ -1,9 +1,8 @@
-import os
 import pandas as pd
 from data.db.engine import engine
 
 def load_transactions():
-    df = pd.read_sql("SELECT * FROM transactions", engine)
+    df = pd.read_sql("SELECT * FROM transactions", con=engine)
     df = df.dropna(subset=["date", "amount"])
     df["date"] = pd.to_datetime(df["date"])
-    return df 
+    return df
